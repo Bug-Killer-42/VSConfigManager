cfd7d11b53c34a344764a201ef4be96ca15cc2a7
bug-killer-42 <jacksongoh254@gmail.com>
2025-05-11 14:03:55 +0800

    fixed install.ps1 bug

--- install.ps1
+++ install.ps1
@@ -1,13 +1,14 @@
-function main {
+
+function main([string]$install_file_name="main") {
     $obj=[UI]::new()
-    [string]$scriptSource = Join-Path $PWD "VSConfigManager.ps1"
+    [string]$scriptSource = Join-Path $PWD "$install_file_name.ps1"
     [string]$Path = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules"
-    [string]$targetFolder = Join-Path $Path "VSConfigManager"
-    [string]$targetScriptPath = Join-Path $targetFolder "VSConfigManager.ps1"
+    [string]$targetFolder = Join-Path $Path "$install_file_name"
+    [string]$targetScriptPath = Join-Path $targetFolder "$install_file_name.ps1"
     [string]$dotCall = ". '$targetScriptPath'"
 
     if (Test-Path $targetFolder) {
-        $obj.data_asking_update()
+        $obj.data_asking_update($install_file_name)
         if($obj.asking() -eq 2){
             return
         }else{
@@ -28,9 +29,9 @@ function main {
     
     if ($profileContent -notmatch [regex]::Escape($dotCall)) {
         Add-Content -Path $PROFILE -Value "`n $dotCall" -Force
-        $obj.data_success()
+        $obj.data_success($install_file_name)
     }else{
-        $obj.data_update_success()
+        $obj.data_update_success($install_file_name)
     }
     $obj.output()
 }
@@ -43,67 +44,67 @@ class UI {
     [string]$title_zh = ""
     [string]$title_zh_tw = ""
     [string]$language = (Get-UICulture).Name
-
-    [void] output() {
-        if ($this.language -like "zh-*") {
-            [string]$output_message = $this.message_zh
-            [string]$output_title = $this.title_zh
-        } else {
-            [string]$output_message = $this.message_en
-            [string]$output_title = $this.title_en
-        }
-        $shell = New-Object -ComObject WScript.Shell
-        $shell.Popup($output_message, 0, $output_title, 0x0) > $null
-        Clear-Host
-    }
-    [int] asking(){
+    [string]$output_message = ""
+    [string]$output_title = ""
+    [void] init() {
+        $this.language = (Get-UICulture).Name
         if ($this.language -like "zh-*") {
             if($this.language -like "zh-TW" || $this.language -like "zh-HK"){
-                [string]$output_message = $this.message_zh_tw
-                [string]$output_title = $this.title_zh_tw
+                [string]$this.output_message = $this.message_zh_tw
+                [string]$this.output_title = $this.title_zh_tw
             }else{
-            [string]$output_message = $this.message_zh
-            [string]$output_title = $this.title_zh
+            [string]$this.output_message = $this.message_zh
+            [string]$this.output_title = $this.title_zh
             }
         }else {
-            [string]$output_message = $this.message_en
-            [string]$output_title = $this.title_en
+            [string]$this.output_message = $this.message_en
+            [string]$this.output_title = $this.title_en
         }
-        $shell = New-Object -ComObject WScript.Shell
-        return $shell.Popup($output_message, 0, $output_title, 0x1)
     }
-    [void] data_success() {
-        $this.message_en = "VSConfigManager installed successfully! It will auto-load on PowerShell startup."
+    [void] output() {
+        $this.init()
+        $this.pop(0x0)
+        Clear-Host
+    }
+    [int] pop([int]$mode){
+        $shell = New-Object -ComObject WScript.Shell
+        return $shell.Popup($this.output_message, 0, $this.output_title, $mode)
+    }
+    [int] asking(){
+        $this.init()
+        return $this.pop(0x1)
+    }
+    [void] data_success([string]$name="main") {
+        $this.message_en = "$name installed successfully! It will auto-load on PowerShell startup."
         $this.title_en = "Installation Complete"
         $this.title_zh = "安装成功"
-        $this.message_zh = "成功安装 VSConfigManager！PowerShell 每次开启时会自动加载此文件。"
-        $this.message_zh_tw = "成功安裝 VSConfigManager！PowerShell 每次開啟時會自動加載此腳本。"
+        $this.message_zh = "成功安装 $name！PowerShell 每次开启时会自动加载此文件。"
+        $this.message_zh_tw = "成功安裝 $name！PowerShell 每次開啟時會自動加載此腳本。"
         $this.title_zh_tw = "安裝成功"
     }
-    [void] data_asking_update() {
-        $this.message_en = "VSConfigManager already installed! Do you want to update it?"
+    [void] data_asking_update([string]$name="main") {
+        $this.message_en = "$name already installed! Do you want to update it?"
         $this.title_en = "Info"
         $this.title_zh = "提示"
-        $this.message_zh = "VSConfigManager 已经安装！是否更新？"
-        $this.message_zh_tw = "VSConfigManager 已經安裝！是否更新？"
+        $this.message_zh = "$name 已经安装！是否更新？"
+        $this.message_zh_tw = "$name 已經安裝！是否更新？"
         $this.title_zh_tw = "提醒"
     }
-    [void] data_update_success() {
-        $this.message_en = "VSConfigManager updated successfully! It will auto-load on PowerShell startup."
+    [void] data_update_success([string]$name="main") {
+        $this.message_en = "$name updated successfully! It will auto-load on PowerShell startup."
         $this.title_en = "Update Complete"
         $this.title_zh = "更新成功"
-        $this.message_zh = "成功更新 VSConfigManager！PowerShell 每次开启时会自动加载此文件。"
-        $this.message_zh_tw = "成功更新 VSConfigManager！PowerShell 每次開啟時會自動加載此腳本。"
+        $this.message_zh = "成功更新 $name！PowerShell 每次开启时会自动加载此文件。"
+        $this.message_zh_tw = "成功更新 $name！PowerShell 每次開啟時會自動加載此腳本。"
         $this.title_zh_tw = "更新成功"
     }
-    [void] data_failed() {
-        $this.message_en = "VSConfigManager installed failed! It already installed!"
+    [void] data_failed([string]$name="main") {
+        $this.message_en = "$name installed failed! It already installed!"
         $this.title_en = "Installation Failed"
         $this.title_zh = "安装失败"
-        $this.message_zh = "安装 VSConfigManager 失败！该文件已存在。"
-        $this.message_zh_tw = "安裝 VSConfigManager 失敗！該檔案已存在。"
+        $this.message_zh = "安装 $name 失败！该文件已存在。"
+        $this.message_zh_tw = "安裝 $name 失敗！該檔案已存在。"
         $this.title_zh_tw = "安裝錯誤"
     }
 }
-
-main
+main -install_file_name "VSConfigManager"
